b0a9fdf250654a0a6f3e8ff42bf475a279009a13
JayasinghePS <127961538+JayasinghePS@users.noreply.github.com>
2025-08-14 20:44:41 +0530

    Modify the chat area part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="min-h-screen w-full bg-[url('./src/assets/bgImage.svg')] bg-cover bg-center">
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
